465e3c0592e43a6a4b2e5a852127947a9a9d648a
Alexa Carrington <acarring@wellesley.edu>
2020-12-07 11:39:47 -0500

    Added function to remove items

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const list = document.querySelector('.list');
 const items = document.querySelector('.items');
 const item = document.querySelectorAll('.item');
+const checkboxes =  document.querySelectorAll('input[type=checkbox]');
 const input = document.querySelector('.add-item input');
 const button = document.querySelector('.button');
 
@@ -12,6 +13,14 @@ function addItem() {
     `;
     newItem.classList.add('item');
     items.appendChild(newItem);
+    input.value = "";
 }
 
+function removeItem (e) {
+    if (e.target.checked === true) {
+        e.target.parentNode.remove(e.target);
+    };
+  }
+
 button.addEventListener('click', addItem);
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', removeItem));
