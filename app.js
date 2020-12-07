fa3da08ec1d67e30dcb5b93e4022754745c42445
Alexa Carrington <acarring@wellesley.edu>
2020-12-07 10:49:27 -0500

    Added selectors and event listener in js, edited styling in css, and edited layout in html

---
+++ app.js
@@ -0,0 +1,11 @@
+const list = document.querySelector('.list');
+const items = document.querySelector('.items');
+const item = document.querySelectorAll('.item');
+const input = document.querySelector('.add-item input');
+const button = document.querySelector('.button');
+
+function addItem() {
+   
+}
+
+button.addEventListener('click', addItem);
