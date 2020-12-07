4f70664924cb69c7eeb2b84feca8a8e7cde9a3a6
Alexa Carrington <acarring@wellesley.edu>
2020-12-07 11:05:50 -0500

    Added function to add list items

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const input = document.querySelector('.add-item input');
 const button = document.querySelector('.button');
 
 function addItem() {
-   
+    let myVal = input.value;
+    let newItem = document.createElement('li');
+    newItem.innerHTML = `
+        <input type="checkbox">${myVal}
+    `;
+    newItem.classList.add('item');
+    items.appendChild(newItem);
 }
 
 button.addEventListener('click', addItem);
